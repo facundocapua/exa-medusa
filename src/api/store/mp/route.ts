8bb91a48aff5c4c37fceb85bc69bd537c09f8366
Facundo Capua <facundocapua@gmail.com>
2024-04-05 15:26:02 -0300

    feat: mercadopago integration

---
+++ src/api/store/mp/route.ts
@@ -0,0 +1,58 @@
+import type {
+  MedusaRequest,
+  MedusaResponse
+} from '@medusajs/medusa'
+import type MercadopagoService from 'src/services/mercadopago'
+
+export async function GET (
+  req: MedusaRequest,
+  res: MedusaResponse
+): Promise<MedusaResponse> {
+  return res.json({
+    message: 'Hello MercadoPago'
+  })
+}
+
+export async function POST (
+  req: MedusaRequest,
+  res: MedusaResponse
+): Promise<MedusaResponse> {
+  const data = req.body
+  // console.log('[MercadoPago Hook]', data)
+  if (req.body.type === 'payment') {
+    const mercadoPagoProviderService: MercadopagoService = req.scope.resolve('mercadopagoService')
+    const mercadoPagoPayment = await mercadoPagoProviderService.retrievePayment({
+      id: data.data.id
+    })
+    const cartId = mercadoPagoPayment.external_reference
+    if (cartId) {
+      const manager = req.scope.resolve('manager')
+      const cartService = req.scope.resolve('cartService')
+      const orderService = req.scope.resolve('orderService')
+      const order = await orderService
+        .retrieveByCartId(cartId, {
+          relations: ['payments']
+        })
+        .catch(() => undefined)
+
+      await manager.transaction(async (m) => {
+        if (!order) {
+          const cartServiceTx = cartService.withTransaction(manager)
+          await cartServiceTx.setPaymentSession(cartId, 'mercadopago')
+          await cartServiceTx.authorizePayment(cartId, {
+            id: mercadoPagoPayment.id
+          })
+          const newOrder = await orderService
+            .withTransaction(manager)
+            .createFromCart(cartId)
+
+          if (mercadoPagoPayment.captured) {
+            await orderService.capturePayment(newOrder.id)
+          }
+        }
+      })
+    }
+  }
+
+  return res.status(200).json(data)
+}
