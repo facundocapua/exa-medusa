c05c69fa05588ac8ee0c5cd81cc062d97e698297
Facundo Capua <facundocapua@gmail.com>
2024-05-06 19:01:29 -0300

    feat: support for multiple stores

--- medusa-config.js
+++ medusa-config.js
@@ -23,15 +23,15 @@ try {
 
 // CORS when consuming Medusa from admin
 const ADMIN_CORS =
-  process.env.ADMIN_CORS ?? 'http://localhost:7000,http://localhost:7001'
+  process.env.ADMIN_CORS || 'http://localhost:7000,http://localhost:7001'
 
 // CORS to avoid issues when consuming Medusa from a client
-const STORE_CORS = process.env.STORE_CORS ?? 'http://localhost:8000'
+const STORE_CORS = process.env.STORE_CORS || 'http://localhost:8000'
 
 const DATABASE_URL =
-  process.env.DATABASE_URL ?? 'postgres://localhost/medusa-starter-default'
+  process.env.DATABASE_URL || 'postgres://localhost/medusa-starter-default'
 
-const REDIS_URL = process.env.REDIS_URL ?? 'redis://localhost:6379'
+const REDIS_URL = process.env.REDIS_URL || 'redis://localhost:6379'
 
 const plugins = [
   'medusa-fulfillment-manual',
