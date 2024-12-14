167444c8410e0e19f0446d41f3fb2d5645c1e91e
Facundo Capua <facundocapua@gmail.com>
2024-12-14 12:38:23 -0300

    feat(order): implement inventory reservation cart orders

--- src/api/store/mp/route.ts
+++ src/api/store/mp/route.ts
@@ -1,6 +1,7 @@
-import type {
-  MedusaRequest,
-  MedusaResponse
+import {
+  type Cart,
+  type MedusaRequest,
+  type MedusaResponse
 } from '@medusajs/medusa'
 import type MercadopagoService from 'src/services/mercadopago'
 
@@ -36,6 +37,9 @@ export async function POST (
       const manager = req.scope.resolve('manager')
       const cartService = req.scope.resolve('cartService')
       const orderService = req.scope.resolve('orderService')
+      const productVariantInventoryService = req.scope.resolve('productVariantInventoryService')
+      const paymentService = req.scope.resolve('paymentService')
+
       const order = await orderService
         .retrieveByCartId(cartId, {
           relations: ['payments']
@@ -45,13 +49,37 @@ export async function POST (
       await manager.transaction(async (m) => {
         if (!order) {
           const cartServiceTx = cartService.withTransaction(manager)
+          const productVariantInventoryServiceTx = productVariantInventoryService.withTransaction(manager)
+          const paymentServiceTx = paymentService.withTransaction(manager)
+
           await cartServiceTx.setPaymentSession(cartId, 'mercadopago')
           await cartServiceTx.authorizePayment(cartId, {
             id: mercadoPagoPayment.id
           })
+          const cart: Cart = await cartService.retrieveWithTotals(cartId, {
+            relations: [
+              'region',
+              'payment',
+              'payment_sessions',
+              'items.variant.product.profiles'
+            ]
+          })
+
+          const reservations = await makeReservations(cart, productVariantInventoryServiceTx)
+          if (reservations.some(([_, error]) => error)) {
+            await removeReservations(reservations, productVariantInventoryServiceTx)
+            paymentServiceTx.cancelPayment(cart.payment)
+
+            await cartServiceTx.update(cart.id, {
+              payment_authorized_at: null
+            })
+
+            throw new Error('Could not reserve inventory')
+          }
+
           const newOrder = await orderService
             .withTransaction(manager)
-            .createFromCart(cartId)
+            .createFromCart(cart)
 
           if (mercadoPagoPayment.captured) {
             await orderService.capturePayment(newOrder.id)
@@ -62,4 +90,57 @@ export async function POST (
   }
 
   return res.status(200).json(data)
+}
+
+async function makeReservations (cart: Cart, productVariantInventoryServiceTx: any): Promise<any> {
+  const reservations = Promise.all(
+    cart.items.map(async (item) => {
+      if (item.variant_id) {
+        try {
+          const inventoryConfirmed =
+            await productVariantInventoryServiceTx.confirmInventory(
+              item.variant_id,
+              item.quantity,
+              { salesChannelId: cart.sales_channel_id }
+            )
+
+          if (!inventoryConfirmed) {
+            throw new Error(`Variant with id: ${item.variant_id} does not have the required inventory`)
+          }
+
+          return [
+            await productVariantInventoryServiceTx.reserveQuantity(
+              item.variant_id,
+              item.quantity,
+              {
+                lineItemId: item.id,
+                salesChannelId: cart.sales_channel_id
+              }
+            ),
+            undefined
+          ]
+        } catch (error) {
+          return [undefined, error]
+        }
+      }
+      return [undefined, undefined]
+    })
+  )
+
+  return await reservations
+}
+
+async function removeReservations (reservations: any, inventoryService: any): Promise<void> {
+  await Promise.all(
+    reservations.map(async ([reservations]) => {
+      if (reservations) {
+        return reservations.map(async (reservation) => {
+          await inventoryService.deleteReservationsByLineItem(
+            reservation.line_item_id
+          )
+        })
+      }
+      await Promise.resolve()
+    })
+  )
 }
